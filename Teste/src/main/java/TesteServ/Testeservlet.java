d153dc0beb23a0f343d50d2213b5a8fb8105a4ca
Pablo de Oliveira <pablo.oliveira.xavier@gmail.com>
2022-11-16 20:31:55 -0300

    Imprimindo mais de uma String/opcao na pagina

--- Teste/src/main/java/TesteServ/Testeservlet.java
+++ Teste/src/main/java/TesteServ/Testeservlet.java
@@ -7,6 +7,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Servlet implementation class Testeservlet
@@ -38,11 +40,14 @@ public class Testeservlet extends HttpServlet {
 		doGet(request, response);
 		String nome = request.getParameter("nome");
 		String idade = request.getParameter("idade");
-		String preferencias = request.getParameter("preferencias");
-		System.out.println(preferencias);
+		String[] preferencias = request.getParameterValues("preferencias");
+		 for (int i = 0; i < preferencias.length; i++) {
+			 System.out.println(Arrays.toString(preferencias));
+		 }
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		out.println("<h1>" +nome + "</h1>"+ "<h2>" + idade + "</h2>" + "<h3>" + preferencias + "</h3>");
+		out.println("<h1>" +nome + "</h1>"+ "<h2>" + idade + "</h2>" + "<h3>" + 
+		Arrays.toString(preferencias).replace("[", "").replace("]", "") + "</h3>");
 		
 	}
 
